dee645602ab002827fceb0c4b1401fc9a92182f1
Walter Luan <lubrias@gmail.com>
2026-01-28 18:02:52 -0300

    Limpando arquivos duplicados da raiz

--- ex001.js
+++
@@ -1,3 +0,0 @@
-window.alert("minha primeira mensagem");
-window.confirm("Está gostando de javaScript?");
-window.prompt("Qual é seu nome");
--- ex002.js
+++
@@ -1,5 +0,0 @@
-var nome = window.prompt("Digite seu nome: ");
-
-document.write(`Seu nome tem ${nome.length} letras, ${nome}.`);
-document.write(`Seu nome em maiúsculas é ${nome.toUpperCase()} </br>`);
-document.write(`Seu nome em minúsculas é ${nome.toLowerCase()}`);
