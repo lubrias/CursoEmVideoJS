75a689abf98bb9d108bebe8dc14744423d61836a
Walter Luan <lubrias@gmail.com>
2026-01-28 17:49:49 -0300

    Meus primeiros códigos em javascript

---
+++ MODULO-A/ex002.js
@@ -0,0 +1,5 @@
+var nome = window.prompt("Digite seu nome: ");
+
+document.write(`Seu nome tem ${nome.length} letras, ${nome}.`);
+document.write(`Seu nome em maiúsculas é ${nome.toUpperCase()} </br>`);
+document.write(`Seu nome em minúsculas é ${nome.toLowerCase()}`);
