732e6326ee40be9bc837c5216d9f88cbba796b36
Walter Luan <lubrias@gmail.com>
2026-02-05 12:05:04 -0300

    Verificador de idade ' '

---
+++ MODULO-A/SEGUNDOSITE/siteIdade.js
@@ -0,0 +1,13 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    
+
+    if (fano.value.length == 0 || fano.value > ano ) {
+        window.alert('ERRO')
+    } else {
+        window.alert('tudo ok')
+    }
+}
