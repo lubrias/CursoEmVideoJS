6347d2f11f2586a5e4167facb899f8a1bcf170a0
Walter Luan <lubrias@gmail.com>
2026-02-09 18:41:12 -0300

    Treinando

--- MODULO-A/PRIMEIROSITE/treinoRelogio.js
+++ MODULO-A/PRIMEIROSITE/treinoRelogio.js
@@ -1,22 +1,32 @@
 function carregar() {
     var div1 = document.getElementById('div1')
     var img = document.getElementById('img')
+    // Transforma esses objetos em objetos para o JS
+
     var data = new Date()
     var hora = data.getHours()
+    // Pega e Transforma as horas em JS
+
     div1.innerHTML = `Agora são ${hora} horas.`
     
 
     if (hora >= 0 && hora < 12) {
         //Manhã
         img.src = 'img/amanhecer.jpg'
+        // Captura sua imagem pq selecionou a div2 la em cima
+
         document.body.style.background = "#e2cd9f" 
     } else if (hora <18) {
         //Tarde
         img.src = 'img/tarde.jpg'
+        // Captura sua imagem pq selecionou a div2 la em cima
+
         document.body.style.background = "#b9846f"
     } else {
         //Noite
         img.src = 'img/noite.jpg'
+        // Captura sua imagem pq selecionou a div2 la em cima
+        
         document.body.style.background = "#515154"
     }
 }
