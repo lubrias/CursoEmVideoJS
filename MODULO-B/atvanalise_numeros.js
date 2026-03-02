1d76b632dc1822f691570b4d2d5dbd93e85be54d
Walter Luan <lubrias@gmail.com>
2026-03-02 19:05:17 -0300

    Analisador de números

--- MODULO-B/atvanalise_numeros.js
+++ MODULO-B/atvanalise_numeros.js
@@ -1,6 +1,6 @@
-    let arrayVazio = []
-    let res = document.getElementById('div2')
-    
+let arrayVazio = []
+let res = document.getElementById('div2')
+
 
 function adicionar() {
     let num = Number(document.getElementById('num').value)
@@ -24,26 +24,26 @@ function finalizar() {
     let soma = 0
 
     //calculos menor e maior
-    for (let i = 1;i< arrayVazio.length;i++) {
+    for (let i = 1; i < arrayVazio.length; i++) {
         if (arrayVazio[i] > maior) {
             maior = arrayVazio[i];
         }
     }
 
-    for (let j = 1; j < arrayVazio.length;j++) {
+    for (let j = 1; j < arrayVazio.length; j++) {
         if (arrayVazio[j] < menor) {
             menor = arrayVazio[j]
         }
     }
-//[1,3,5]
+    //[1,3,5]
     //calculos soma
-    for (let k = 0; k < arrayVazio.length;k++) {
+    for (let k = 0; k < arrayVazio.length; k++) {
         soma += arrayVazio[k]
     }
 
     //calculo media
     media = soma / arrayVazio.length
-    
+
     res.innerHTML += `<p>Ao todo, temos ${arrayVazio.length} números registrados.</p>`
     res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
     res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
